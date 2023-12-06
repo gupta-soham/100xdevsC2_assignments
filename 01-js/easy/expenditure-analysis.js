8ce3659ad9b4f0ca4517b1bfee5532f8bc248eea
Soham Gupta <soham.gupta003@gmail.com>
2023-12-06 23:55:41 +0530

    Solutions for W1 easy questions

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -14,7 +14,33 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  const categoryTotals = {};
+
+  transactions.forEach(transaction => {
+    const { category, price } = transaction;
+
+    if (categoryTotals[category]) 
+      categoryTotals[category] += price; // If the category already exists, add the price to the total
+
+    else 
+      categoryTotals[category] = price; // If the category does not exist, initialize it with the current price
+
+  });
+  
+  const result = [];
+  for (const category in categoryTotals) {
+    result.push({ [category]: categoryTotals[category] });
+  }
+
+  return result;
 }
 
 module.exports = calculateTotalSpentByCategory;
+
+// Testing
+// const transactions = [
+//   { itemName: 'Item1', category: 'Groceries', price: 50 },
+//   { itemName: 'Item2', category: 'Electronics', price: 200 },
+//   { itemName: 'Item3', category: 'Stationary', price: 30 }
+// ];
+// console.log(calculateTotalSpentByCategory(transactions));
