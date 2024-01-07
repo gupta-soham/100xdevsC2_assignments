117b8fb034cc3c55b6c2508ae61ed4e78f0b0796
Soham Gupta <soham.gupta003@gmail.com>
2024-01-07 23:48:58 +0530

    Added solutions for W3 `MongoDB`

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -1,19 +1,31 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect('mongodb+srv://admin:j5AC2xXek5LDd2DJ@cluster0.akf5gyy.mongodb.net/');
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
+    username: String,
+    password: String,
 });
 
 const UserSchema = new mongoose.Schema({
     // Schema definition here
+    username: {type: String, required: true},
+    password: String,
+    purchasedCourses: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Course'
+    }],
 });
 
 const CourseSchema = new mongoose.Schema({
     // Schema definition here
+    title: String,
+    description: String,
+    price: Number, 
+    imgLink: String
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
--- week-3/03-mongo/routes/admin.js
+++ week-3/03-mongo/routes/admin.js
@@ -1,18 +1,74 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const { Admin, Course } = require("../db");
 const router = Router();
 
 // Admin Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     // Implement admin signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // Check if the user already exists
+    // Admin.create({
+    //     username: username,
+    //     password: password
+    // })
+    // .then(() => {
+    //     res.json({
+    //         message: 'Admin created successfully'
+    //     })
+    // })
+    // .catch((err) => {
+    //     res.json({
+    //         message: 'Admin not created'
+    //     })
+    // })
+
+    // Async-await Syntax:
+    await Admin.create({
+        username: username,
+        password: password
+    })
+    res.json({
+        message: "Admin created successfully"
+    })
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
+router.post('/courses', adminMiddleware, async (req, res) => {
     // Implement course creation logic
+    const title = req.body.title;
+    const description = req.body.description;
+    const imgLink = req.body.imgLink;
+    const price = req.body.price;
+
+    const newCourse = await Course.create({
+        title, // Since keay & value are the same
+        description,
+        price,
+        imgLink
+    })
+    res.json({
+        message: "Course created successfully",  
+        courseId: newCourse._id
+    })
 });
 
-router.get('/courses', adminMiddleware, (req, res) => {
+router.get('/courses', adminMiddleware, async (req, res) => {
     // Implement fetching all courses logic
+    const data = await Course.find({});
+    res.json({
+        courses: data
+    })
+     
+    // Using promises 👇
+    // Course.find({});
+    //     .then((data) => {
+    //         res.json({
+    //             courses: data
+    //         })
+    // })
+
 });
 
 module.exports = router;
